2ce4bed550f225f2447e14ac61c46c9891d70a8f
Prtstream820894 <153801810+Prtstream820894@users.noreply.github.com>
2026-05-03 18:59:38 +0530

    Update default cookie value in get.js

--- api/get.js
+++ api/get.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const default_cookie = "hdntl=exp=1777766439~acl=%2f*~id=e0c17f7994f8b1a2dcb3ba81cdbdfe5e~data=hdntl~hmac=a48b46c5516d4c6a2925c38426b5bd7ba20211c257764369f64064cd9e34acb4";
+  const default_cookie = "hdntl=exp=1777852838~acl=%2f*~id=07b8e1e4ae3e8f83e968779e7e6fbc12~data=hdntl~hmac=ebfa429a6d07e78b739703111d3f4277bdfe26613c4c6960fe2c018a4e425c7f";
 
   const url = "https://server.vodep39240327.workers.dev/channel/raw?=m3u";
 
