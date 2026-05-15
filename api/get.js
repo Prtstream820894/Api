afcd41e9df5d619a0f4c7621908ab5d95734d56b
Prtstream820894 <153801810+Prtstream820894@users.noreply.github.com>
2026-05-15 17:43:53 +0530

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const default_cookie = "hdntl=exp=1778846438~acl=%2f*~id=abc760326a21f987956e49e7d27577db~data=hdntl~hmac=cb3b8d0e28b77e07e090935a3aca5c51e23e03365b0a059b26fe97a0ba1ba2bb";
+  const default_cookie = "hdntl=exp=1778932850~acl=%2f*~id=931ee02dc514c81ce1266fa64728acd7~data=hdntl~hmac=55d8adcde90de57893572cf5051402e2adbfe5144eb56385294d17b62f35ef66";
 
   const url = "https://server.vodep39240327.workers.dev";
 
