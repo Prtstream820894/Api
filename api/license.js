14d82600afa8d31b82eb726041cddfd3db39ebd5
Prtstream820894 <153801810+Prtstream820894@users.noreply.github.com>
2026-05-18 15:19:17 +0530

    Update license.js

--- api/license.js
+++ api/license.js
@@ -1,21 +1,27 @@
 export default async function handler(req, res) {
-  // Isse player ko JSON mil jayega
+  // Dittoo JSON format ke liye headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
 
   const { id } = req.query;
-  if (!id) return res.status(400).json({ error: "No ID" });
+  if (!id) return res.status(400).json({ error: "Missing Channel ID" });
 
   try {
-    // Asli license server jahan se JSON mil raha hai
     const targetUrl = `https://tplay.virey40690.workers.dev/key/${id}`;
     
-    const response = await fetch(targetUrl);
+    const response = await fetch(targetUrl, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+      }
+    });
+
+    if (!response.ok) throw new Error("Source JSON fetch failed");
+
     const data = await response.json();
 
-    // Same to same wahi JSON return kar rahe hain jo asli server ka hai
+    // Poora ka poora JSON waisa ka waisa hi return kar rahe hain
     return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({ error: "License Fetch Failed" });
+    return res.status(500).json({ error: error.message });
   }
 }
