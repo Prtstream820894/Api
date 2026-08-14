7d116e76854fc28bddf68826fd42d1ea815687a8
Prtstream820894 <153801810+Prtstream820894@users.noreply.github.com>
2026-08-14 21:24:34 +0530

    Add NCC playlist URL and update fetch timeouts
    Added a new URL for NCC playlist with a timeout.

--- api/indexplay.js
+++ api/indexplay.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
     const url6 = "https://sonyliv-event-5e05.poonamchouhan076.workers.dev/";
     const url7 = "https://old-shape-1bd3.poonamchouhan076.workers.dev/";
     const url8 = "https://bitter-recipe-3d25.poonamchouhan076.workers.dev/"; // Nayi playlist add ki gayi hai
+    const url9 = "https://server-iota-nine-71.vercel.app/api/ncc"; // Nayi NCC playlist add ki gayi hai
   
     
 
@@ -65,7 +66,8 @@ export default async function handler(req, res) {
       fetchWithTimeout(url5, 8000),
       fetchWithTimeout(url6, 8000),
       fetchWithTimeout(url7, 8000),
-      fetchWithTimeout(url8, 30000), // Nayi URL ke liye timeout add kiya gaya hai
+      fetchWithTimeout(url8, 30000),
+      fetchWithTimeout(url9, 10000), // Nayi URL ke liye timeout add kiya gaya hai
     ]);
 
     if (!responses[0]) {
